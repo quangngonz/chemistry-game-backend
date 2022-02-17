d12c17e802b9dd8ea342fab95a74cbe1497dc803
quangngonz <46818455+quangngonz@users.noreply.github.com>
2022-02-17 23:53:58 +0700

    ready to deploy

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const fs = require("fs");
 const app = express();
 
 app.get("/", (req, res) => {
+  res.status(200).send("Hi").end();
+});
+
+app.get("/getScore", (req, res) => {
   const jsonData = fs.readFileSync("./scores.json");
   const data = JSON.parse(jsonData);
+  res.status(200).send(data).end();
+});
+
+// recive name and score and save it to json file
+app.post("/sendScore", (req, res) => {
+  const jsonData = fs.readFileSync("./scores.json");
+  const data = JSON.parse(jsonData);
+
+  const name = req.body.name;
+  const score = req.body.score;
+
+  data.push({ name, score });
+  fs.writeFileSync("./scores.json", JSON.stringify(data));
+
   res.status(200).send(data).end();
 });
 
