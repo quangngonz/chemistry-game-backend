79ced7ed12d27b701582346719e61fefe3858239
quangngonz <46818455+quangngonz@users.noreply.github.com>
2022-02-19 16:42:34 +0700

    clear json file

--- index.js
+++ index.js
@@ -50,5 +50,4 @@ app.post("/sendScore", (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-  console.log("Press Ctrl+C to quit.");
 });
